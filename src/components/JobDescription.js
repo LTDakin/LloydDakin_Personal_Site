a6338212abd1239db0fe1e925cd094afa6efe454
Lloyd Dakin <taidakin99lol@gmail.com>
2025-11-13 17:40:07 -0800

    change color to blue

--- src/components/JobDescription.js
+++ src/components/JobDescription.js
@@ -2,12 +2,15 @@ import React from "react";
 
 function JobDescription(props) {
   const title = props.title;
+
   const tech = props.tech.map((techItem) => (
     <span className="job-tech-item">{techItem}</span>
   ));
+
   const points = props.points.map((point) => (
     <p className="job-point">{point}</p>
   ));
+  
   return (
     <div className="job-description-component">
       <h2 className="job-title">{title}</h2>
