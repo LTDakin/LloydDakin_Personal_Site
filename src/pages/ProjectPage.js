40d3d1dbcef824b3247eefe2c0aec6ccbf6cd472
Lloyd Dakin <taidakin99lol@gmail.com>
2026-09-03 13:59:07 -0700

    Update Resume, move projects into an editable json config

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import BackButton from '../components/BackButton';
 import ProjectCard from '../components/ProjectCard';
+import projects from '../assets/projects.json';
 
 function ProjectPage(props) {
+  const activeProjects = projects.filter(project => project.active);
+
   return (
     <div className="project-page-div">
       <div className="page-header d-flex justify-content-between">
@@ -11,31 +14,14 @@ function ProjectPage(props) {
       </div>
       <div className="project-cards-div-container">
         <div className="project-cards-div">
-          <ProjectCard
-            cardTitle="Centroid Algorithim"
-            projectDescription="A Euclidian clustering algorithim that takes in a list of tuples that represent points in a 2D space. The algorithim then proceeds to group the points using Euclidian distance and assigns centroids to each group. A centroid is the mean middle of all the points in a group"
-            githubLink="https://github.com/LTDakin/ClusteringAlgorithim"
-          ></ProjectCard>
-          <ProjectCard
-            cardTitle="Shoot-Planner"
-            projectDescription="A Full Stack web application project. Designed to create a social media site for photographers that lets them create accounts, add photos, and plan shoots"
-            githubLink="https://github.com/LTDakin/Shoot-Planner-Web-App"
-          ></ProjectCard>
-          <ProjectCard
-            cardTitle="Instagrown"
-            projectDescription="A social networking site for plant owners. Complete with account creation and login, profiles, posts, comments, and messages."
-            githubLink="https://github.com/LTDakin/InstaGrown"
-          ></ProjectCard>
-          <ProjectCard
-            cardTitle="IBM Watson Query Engine"
-            projectDescription="This is a NLP program that parses wikipedia articles using Lucene to then be able to answer a file containing questions about historic events, places, and people. First the knowledge base is generated and then you can pass the question text file to recieve answers."
-            githubLink="https://github.com/LTDakin/IBM_Watson_jeopardy_query_engine"
-          ></ProjectCard>
-          <ProjectCard
-            cardTitle="TikTok Partnership for Shopify Apps"
-            projectDescription="Currently WIP this shopify app would parse store items and then use the chatgpt api to reccomend tik tok creatives that the store owner could send automated emails with offers to do product promotions"
-            githubLink="https://github.com/LTDakin/____-shopify-app"
-          ></ProjectCard>
+          {activeProjects.map(project => (
+            <ProjectCard
+              key={project.cardTitle}
+              cardTitle={project.cardTitle}
+              projectDescription={project.projectDescription}
+              githubLink={project.githubLink}
+            />
+          ))}
         </div>
       </div>
     </div>
