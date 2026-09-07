a703816f7314bdf9211bd02857105717bb19c14c
Lloyd Dakin <taidakin99lol@gmail.com>
2026-09-06 20:53:15 -0700

    refactor projects page components out of components.css and delete components.css

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PageHeader from '../components/PageHeader';
 import ProjectCard from '../components/ProjectCard';
 import projects from '../assets/files/projects.json';
+import styled from 'styled-components';
 
 function ProjectPage(props) {
   const activeProjects = projects.filter(project => project.active);
@@ -9,20 +10,26 @@ function ProjectPage(props) {
   return (
     <div className="project-page-div">
       <PageHeader title="Projects" />
-      <div className="project-cards-div-container">
-        <div className="project-cards-div">
-          {activeProjects.map(project => (
-            <ProjectCard
-              key={project.cardTitle}
-              cardTitle={project.cardTitle}
-              projectDescription={project.projectDescription}
-              githubLink={project.githubLink}
-            />
-          ))}
-        </div>
-      </div>
+      <ProjectCardDiv>
+        {activeProjects.map((project) => (
+          <ProjectCard
+            key={project.cardTitle}
+            cardTitle={project.cardTitle}
+            projectDescription={project.projectDescription}
+            githubLink={project.githubLink}
+          />
+        ))}
+      </ProjectCardDiv>
     </div>
   );
 }
+
+const ProjectCardDiv = styled.div`
+  margin-left: 40px;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 0;
+  align-content: space-around;
+`;
 
 export default ProjectPage;
