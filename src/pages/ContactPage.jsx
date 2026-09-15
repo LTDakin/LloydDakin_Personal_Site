7c2fe8ec0560614bd40c3bf84b98b9745cd9b985
Lloyd Dakin <taidakin99lol@gmail.com>
2026-09-14 20:07:07 -0700

    contact page styling changes

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,86 +1,23 @@
-import { useRef } from 'react';
 import styled from 'styled-components';
 import PageHeader from '../components/PageHeader';
 
-const EmailForm = styled.form`
-  font-family: coolvetica;
-  padding: 40px;
-`;
-
-const Label = styled.label`
-  font-size: 2em;
-  margin-top: 20px;
-`;
-
-const TextArea = styled.textarea`
-  width: 100%;
-  height: 40%;
-  border: var(--off-black) solid 3px;
-  min-height: 300px;
-  padding: 5px;
-  transition: var(--transition-speed);
-
-  &:focus {
-    outline: none;
-    background-color: var(--off-black);
-    color: var(--off-white);
-  }
-`;
-
-const Input = styled.input`
-  width: 100%;
-  border: var(--off-black) solid 3px;
-  padding: 5px;
-  transition: var(--transition-speed);
-
-  &:focus {
-    outline: none;
-    background-color: var(--off-black);
-    color: var(--off-white);
-  }
-`;
-
-const SubmitInput = styled.input`
-  background-color: var(--off-black);
-  color: var(--off-white);
-  font-size: 2em;
-  padding-top: 5px;
-  padding-bottom: 5px;
-  padding-left: 60px;
-  padding-right: 60px;
-  border: var(--off-black) solid 3px;
-  transition: var(--transition-speed);
-  margin-top: 40px;
-
-  &:hover {
-    color: var(--off-black);
-    background-color: var(--off-white);
-  }
-`;
-
 function ContactPage() {
-  const form = useRef();
-
-  const sendEmail = (e) => {
+  function sendEmail(e) {
     e.preventDefault();
-
     console.log('Email disabled for now');
   };
 
   return (
     <>
       <PageHeader title="Let's connect" />
-      <EmailForm ref={form} onSubmit={sendEmail}>
-        <Label>Name</Label>
+      <EmailForm onSubmit={sendEmail}>
+        <label>Name</label>
         <Input type="text" name="user_name" />
-
-        <Label>Email</Label>
+        <label>Email</label>
         <Input type="email" name="user_email" />
-
-        <Label>Message</Label>
+        <label>Message</label>
         <TextArea name="message" />
-
-        <SubmitInput
+        <SubmitButton
           id="sendButton"
           className="form-submit-button"
           type="submit"
@@ -90,5 +27,49 @@ function ContactPage() {
     </>
   );
 }
+
+const EmailForm = styled.form`
+  font-family: coolvetica;
+  padding: 40px;
+  padding-top: var(--base-padding);
+
+  label {
+    font-size: 2em;
+    margin-top: 20px;
+  }
+`;
+
+const Input = styled.input`
+  width: 100%;
+  border: var(--off-black) solid 3px;
+  padding: 5px;
+  margin-bottom: var(--base-margin);
+  transition: var(--transition-speed);
+
+  &:focus {
+    outline: none;
+    background-color: var(--off-black);
+    color: var(--off-white);
+  }
+`;
+
+const TextArea = styled(Input).attrs({ as: 'textarea' })`
+  height: 40%;
+  min-height: 300px;
+`;
+
+const SubmitButton = styled(Input)`
+  width: auto;
+  background-color: var(--off-black);
+  color: var(--off-white);
+  font-size: 2em;
+  font-weight: bolder;
+  padding: 10px 60px;
+
+  &:hover {
+    color: var(--off-black);
+    background-color: var(--off-white);
+  }
+`;
 
 export default ContactPage;
